8b45802b58bcd4e2bdad179314367a6ff3374928
IIAXSISII <sgautam@triplelift.com>
2025-10-09 22:13:11 -0500

    TLCE-5749 - Update tl version and add script to upload jars to ops bucket for redpanda-connect build to refrence it.

---
+++ kafka-libs-uploader.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Uploads all kafka and connect jars in the current directory to S3
+# Usage: ./kafka-libs-uploader.sh
+# Script needs to be run in the top level directory of the kafka folder once built
+VERSION=3.2.6-tl
+S3_BASE_LOCATION=s3://ops.triplelift.net/public/kafka/${VERSION}/
+for jar in $(find . -name "*.jar" | grep -i connect-); do aws s3 cp $jar s3://ops.triplelift.net/public/kafka/${VERSION}/$(basename $jar); done
+for jar in $(find . -name "*.jar" | grep -i kafka); do aws s3 cp $jar s3://ops.triplelift.net/public/kafka/${VERSION}/$(basename $jar); done
