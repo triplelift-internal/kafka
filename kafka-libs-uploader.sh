5ee0214898661d49c69345f72769e68d2812ab32
Sabin <sgautam@triplelift.com>
2025-10-12 11:46:10 -0500

    Update kafka-libs-uploader.sh
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- kafka-libs-uploader.sh
+++ kafka-libs-uploader.sh
@@ -18,7 +18,7 @@
 # Uploads all kafka and connect jars in the current directory to S3
 # Usage: ./kafka-libs-uploader.sh
 # Script needs to be run in the top level directory of the kafka folder once built
-VERSION=3.2.6-tl
+VERSION=3.6.2-tl
 S3_BASE_LOCATION=s3://ops.triplelift.net/public/kafka/${VERSION}
 
 # Create a temporary directory for unique jars
